dd9cdb0da2d5234c5ea73316a872ac300c94085d
DericM <dmccadden@bengaled.com>
2016-10-30 14:34:34 -0700

    stuff

--- app/src/main/java/ca/bcit/dmccadden/comp3717_signpost/MainActivity.java
+++ app/src/main/java/ca/bcit/dmccadden/comp3717_signpost/MainActivity.java
@@ -1,5 +1,6 @@
 package ca.bcit.dmccadden.comp3717_signpost;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent myIntent = new Intent(this, TabActivity.class);
+        startActivity(myIntent);
+
     }
 }
